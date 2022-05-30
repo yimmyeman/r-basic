5bb3f4a848d2e2bff11a845afb973f7c90d7690c
Yimmy Eman <yimmy.eman@gmail.com>
2022-05-30 16:06:42 -0400

    Update

---
+++ scripts/tema1/Tarea.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 07:36:20 2022
+
+@author: yimmy
+"""
+
+# Escribe una secuencia de instrucciones que permitan leer un número real por pantalla y que muestre si el
+# número es positivo o no.
+
+numero = input(print("Introduce un Número: "))
